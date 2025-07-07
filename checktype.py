92d023c0cc3abc405b956d23a6f10827ab92b925
devarsh170407 <devarshbhatt1747@gmail.com>
2025-07-07 11:38:21 +0530

    new

---
+++ checktype.py
@@ -0,0 +1,7 @@
+L1=[5,6.8,6+6j]
+print(L1[0])
+print(L1[1])
+print(L1[2])
+print(type(L1[0]))
+print(type(L1[1]))
+print(type(L1[2]))
---
+++ numerices.py
@@ -0,0 +1,11 @@
+x=5
+y=10
+
+sum=x+y
+difference= x - y
+product=x*y
+division=x/y
+print ("sum=",sum)
+print("diffrence=",difference)
+print("product=",product)
+print("division=",division)
