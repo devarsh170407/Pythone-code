4ba51c51011cc5f7b1fcc6bbdb8a19b512e1234f
devarsh170407 <devarshbhatt1747@gmail.com>
2025-11-27 04:10:02 +0530

    new

---
+++ project/bulkfile renamer.py
@@ -0,0 +1,14 @@
+import os
+
+def files(dir):
+    files = os.listdir(dir)
+    n = 1
+    new = "project"
+
+    for file in files:
+        newname = f"{new}{n}"  
+        os.rename(os.path.join(dir, file), os.path.join(dir, newname))
+        n += 1
+    print("task done")
+
+files(r"D:\New folder")
